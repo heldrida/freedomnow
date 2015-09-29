ad81a107b3e7721f8d28c63e7d5a2962ece104f3
Helder <heldrida@gmail.com>
2015-09-30 00:23:30 +0100

    added mixin library. changed a few styles, adding drop shadow to the main top box

--- js/app.js
+++ js/app.js
@@ -19,16 +19,14 @@
 
 		setPhotoBoxesSize: function () {
 
-			console.log('setPhotoBoxesSize');
-
 			var docWidth = document.body.clientWidth,
 				w = docWidth / 4;
 
-				console.log(w);
 			for (var i = 0; i < this.photoBoxes.length; i++) {
 
 				this.photoBoxes[i].style.width = w + 'px';
 				this.photoBoxes[i].style.height = w + 'px';
+			
 			}
 
 		},
