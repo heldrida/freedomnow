bfdfc7eb8d089d6ed9f85725322101b45da8da17
Helder <helder@lamoulade.com>
2015-09-29 18:34:58 +0100

    created initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+/**
+ * gulp
+ * $ npm install gulp gulp-livereload gulp-watch gulp-sass gulp-rename gulp-plumber --save-dev
+ */
+
+// Load plugins
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber');
+
+// Sass
+gulp.task('sass', function () {
+    return gulp.src('sass/*.scss')
+           .pipe(plumber())
+           .pipe(sass())
+           .pipe(gulp.dest('./css'));
+});
+
+// Watch
+gulp.task('watch', function () {
+
+    // Watch `html`
+    gulp.watch(['*.php', 'css/*.css', 'js/*.js']).on('change', livereload.changed);
+
+    // Watch `scss` files
+    gulp.watch('sass/*.scss', ['sass']);
+
+    // Create liveReload server
+    livereload.listen();
+
+});
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Freedom Now!</title>
+
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body data-lang="en">
+
+	<div class="freedomnow">
+
+		<div class="clock">
+
+			<div class="row r1">
+
+				<div class="col col-1">
+					<p>101</p>
+				</div>
+				<div class="col col-2">
+					<p>dias presos</p>
+				</div>
+
+			</div>
+
+			<div class="row r2">
+
+				<div class="col col-1">
+					<p>9</p>
+				</div>
+				<div class="col col-2">
+					<p>de greve<br>de fome</p>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
