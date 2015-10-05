bee6d92de0e123444f76f8abe003586734dbb97e
Helder <heldrida@gmail.com>
2015-10-06 00:34:28 +0100

    created privacy policy

---
+++ privacy-policy.php
@@ -0,0 +1,88 @@
+<?php 
+
+	header('Access-Control-Allow-Origin: liberdade-ja.com');
+	header('Access-Control-Allow-Origin: www.liberdade-ja.com');
+
+	include_once('includes/helperFns.php')
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Contra as prisões políticas dos 15 activistas angolanos. Pela liberdade e a democracia em Angola. Freedom for the Political Prisoners in Angola.">
+	<meta name="keywords" content="Luaty da Silva Beirão, Manuel Chivonde (Nito Alves), Nuno Álvaro Dala, Afonso Mahenda Matias (Mbanza Hanza), Nelson Dibango Mendes dos Santos, Itler Jessy Chivonde (Itler Samussuko), Albano Evaristo Bingocabingo, Sedrick Domingos de Carvalho, Fernando António Tomás (Nicolas o Radical), Arante Kivuvu Italiano Lopes, Benedito Jeremias, José Gomes Hata (Cheick Hata), Inocêncio António de Brito ">
+	<title>Liberdade aos Presos Políticos em Angola</title>
+
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body data-lang="en">
+
+	<div class="freedomnow">
+
+		<div class="privacy-policy">
+
+			<p>This Privacy Policy governs the manner in which Liberdade ja! collects, uses, maintains and discloses information collected from users (each, a "User") of the http://liberdade-ja.com website ("Site").</p>
+
+			<h3>Personal identification information</h3>
+			<p>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, fill out a form, and in connection with other activities, services, features or resources we make available on our Site.Users may be asked for, as appropriate, name, email address. Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+
+			<h3>Non-personal identification information</h3>
+			<p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
+
+			<h3>Web browser cookies</h3>
+			<p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+
+			<h3>How we use collected information</h3>
+			<p>Liberdade ja! may collect and use Users personal information for the following purposes:</p>
+			<ul>
+			  <li>
+			    <i>To run and operate our Site</i><br/>
+			    We may need your information display content on the Site correctly.
+			  </li>
+			  <li>
+			    <i>To run a promotion, contest, survey or other Site feature</i><br/>
+			    To send Users information they agreed to receive about topics we think will be of interest to them.
+			  </li>
+			  <li>
+			    <i>To send periodic emails</i><br/>
+			    We may use the email address to send User information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests. 
+			  </li>
+			</ul>
+
+			<h3>How we protect your information</h3>
+			<p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
+
+			<h3>Sharing your personal information</h3>
+			<p>We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. </p>
+
+			<h3>Third party websites</h3>
+			<p>Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licencors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website's own terms and policies.</p>
+
+			<h3>Changes to this privacy policy</h3>
+			<p>Liberdade ja! has the discretion to update this privacy policy at any time. When we do, we will post a notification on the main page of our Site. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+
+			<h3>Your acceptance of these terms</h3>
+			<p>By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes. This policy was generated using <a href="http://privacypolicies.com" target="_blank">PrivacyPolicies.Com</a></p>
+
+			<h3>Contacting us</h3>
+			<p>If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us.</p>
+
+			<p>This document was last updated on October 05, 2015</p>
+
+
+		</div>
+
+	</div>
+
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.1.8/imagesloaded.pkgd.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
+	<script src="js/vendor/validate.min.js"></script>
+	<script src="js/app.js"></script>
+
+</body>
+</html>
