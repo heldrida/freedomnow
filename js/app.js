f515ff77bc23562d9d02f6e1b72efe85d85b86dd
Helder <helder@lamoulade.com>
2015-10-09 19:08:48 +0100

    small amend for mobile, the image fit does not work properly, so its disabled for mobile

--- js/app.js
+++ js/app.js
@@ -120,7 +120,7 @@
 
 				// set background image
 				if (this.photoBoxes[i].getAttribute('class').indexOf('photo-popup') > -1) {
-					//this.photoBoxes[i].style.backgroundImage = 'url(' + this.photoBoxes[i].querySelector('img').getAttribute('src') + ')';
+					this.photoBoxes[i].style.backgroundImage = 'url(' + this.photoBoxes[i].querySelector('img').getAttribute('src') + ')';
 					// disabled for the moment, needs to remove display:none from css img
 					this.imageFit.call(this, this.photoBoxes[i]);
 				}
