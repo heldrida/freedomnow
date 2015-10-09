bc15f49efe71759de093f0b992241bd8f4752b06
Helder <helder@lamoulade.com>
2015-10-09 12:07:31 +0100

    not going to use orm

--- tracker.php
+++ tracker.php
@@ -1,4 +1,15 @@
 <?php
 
-	require_once 'includes/idiorm.php';
 	require_once 'includes/db_credentials.php';
+
+	/*
+	CREATE TABLE IF NOT EXISTS `wp_freedomnow`.`appeals` (
+		`id` INT NOT NULL AUTO_INCREMENT ,
+		`name` VARCHAR(255) NOT NULL ,
+		`email` VARCHAR(255) NOT NULL ,
+		`date` TIMESTAMP NOT NULL ,
+		`fb_uid` INT NOT NULL ,
+		`share_fb` BOOLEAN NOT NULL ,
+		`share_email` BOOLEAN NOT NULL ,
+		PRIMARY KEY (`id`)) ENGINE = InnoDB;
+	*/
